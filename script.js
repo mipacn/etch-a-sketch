a36ee37e0c11d3ee1a82041da7d29f334308c9d7
evorty <138242399+mipacn@users.noreply.github.com>
2024-12-03 12:27:02 +0100

    finished

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-
+let container = document.querySelector(".container2")
 let btn = document.querySelector("button")
 
 btn.addEventListener("click", () => {
-    let size = prompt("Enter a grid size")
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+
+    let size = prompt("Enter a grid size (max:100)")
+    
 
     if (size === null || size.trim() === "") {
         alert("Cancelled")
         return
     }
 
-    while (isNaN(size) || size.trim() === "") {
+    while (isNaN(size) || size.trim() === "" || size > 100) {
         size = prompt("You have to enter a valid number")
 
         if (size === null || size.trim() === "") {
@@ -18,5 +23,16 @@ btn.addEventListener("click", () => {
             return
         }
     }
+
+    let i = 0
+    while(i < size ** 2){
+        let div = document.createElement("div")
+        container.append(div)
+        let prop = 100 / size
+        div.style.cssText = `display: flex; box-sizing: border-box; flex: 1 1 ${prop}%; aspect-ratio: 1 / 1; justify-content: center; align-items: center`
+        i++
+        div.classList.add("grid")
+    }
+    
 })
 
